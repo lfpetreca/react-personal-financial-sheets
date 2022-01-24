c1b113b876f2f823ad36b4321f12f999fcda8922
Luis Fernando M. Petreca <lfpetreca02@gmail.com>
2022-01-24 16:27:11 -0300

    Adding Chart component

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
 import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChart';
 import './Expenses.css';
 
 const Expenses = (props) => {
@@ -19,7 +20,7 @@ const Expenses = (props) => {
     return (
         <Card className='expenses'>
             <ExpensesFilter selected={filteredYear} oChangeFilter={filterChangeHandler} />
-
+            <ExpensesChart expenses={filteredExpenses} />
             <ExpensesList items={filteredExpenses} />
         </Card>
     );
